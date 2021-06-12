eb1fa030906013cfe8cb035e3e299751189f3f25
michaelGuo1204 <2236536804@qq.com>
2021-06-12 11:15:17 +0800

    Update

--- CS-179-Project-master/main.cpp
+++ CS-179-Project-master/main.cpp
@@ -7,8 +7,8 @@ int main(int argc, char *argv[]) {
     float optimized_arguments[2];
     float optimized_fitness[2];
     optimize(1000000, optimized_arguments, optimized_fitness, {
-        func(0, args[0] * (args[0] - args[1] - 2));
-        func(1, args[1] * (args[1] - args[0] - 8));
+        func(0, (args[0]-1)*(args[0]-1)+(args[0]-args[1])*(args[0]-args[1]))//args[0] * (args[0] - args[1] - 2));
+        func(1, (args[1]-3)*(args[1]-3)+(args[0]-args[1])*(args[0]-args[1]))//args[1] * (args[1] - args[0] - 8));
     });
     
     printf("optimal: (");
